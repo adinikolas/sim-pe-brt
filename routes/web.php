4f04c49ef2636c0124f8d9bb2361c86402ba6f50
Nikolas Adi <vinsenniko37@gmail.com>
2026-02-05 09:50:41 +0700

    update structure

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/aduan', AduanController::class);
+
+    Route::get('/export', [ExportController::class, 'excel'])
+        ->name('export.excel');
 });
 
 require __DIR__.'/auth.php';
+
