3cc35c9bc689c00385122c530969fd01ef6365d5
Nikolas Adi <vinsenniko37@gmail.com>
2026-02-24 11:59:27 +0700

    change seeder at MasterJenisAduanSeeder

--- database/seeders/MasterJenisAduanSeeder.php
+++ database/seeders/MasterJenisAduanSeeder.php
@@ -9,29 +9,79 @@ class MasterJenisAduanSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('master_jenis_aduans')->insert([
+        DB::table('master_jenis_aduans')->insertOrIgnore([
             [
-                'nama_aduan' => 'Komplain Pelayanan Pengemudi',
+                'nama_aduan' => 'Komplain Pelayanan Pengemudi (tidak merapat halte, melebihi batas kecepatan, melanggar lalin, merokok, bermain hp, dll)',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nama_aduan' => 'AC Tidak Dingin',
+                'nama_aduan' => 'Komplain Pelayanan Petugas BRT (kurang ramah, tidak sopan, kurang inisiatif, dll)',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nama_aduan' => 'Bus Terlambat',
+                'nama_aduan' => 'Komplain Kondisi Armada (ac, pintu, mesin, dll)',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nama_aduan' => 'Kondisi Bus Tidak Layak',
+                'nama_aduan' => 'Komplain Kondisi Halte/Rambu (rusak, kotor, kurang nyaman, dll)',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nama_aduan' => 'Informasi Tidak Jelas',
+                'nama_aduan' => 'Komplain Interval Armada dan Waktu Pelayanan',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Komplain Tiket (tidak dapat kembalian, mesin error, tarif tidak sesuai, dll)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Komplain Jalur Tidak Sesuai (ngelop)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Komplain Rute (memutar tidak efisien)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Komplain PNP Penuh',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Komplain Aplikasi / Sosial Media',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Saran Penambahan Jalur Khusus BRT',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Saran Penambahan Halte/Rambu',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Saran Penambahan Armada',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Saran Penambahan Jam Layanan',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_aduan' => 'Saran Penambahan Rute',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
